c3f81b13b00bd4fdb5b206c0fe3f83cce7991894
Yonghao Shu <123shuyonghao@gmail.com>
2023-01-10 23:18:08 -0800

    Vue event demo

---
+++ vue/simple-single-page-event/README.md
@@ -0,0 +1,12 @@
+# Notes
+
+v-bind: set the value of an attribute.  
+```
+<a v-bind:href="vueLink">about Vue</a>
+<!-- vueLink is part of data -->
+```
+v-on: bind the event handler.
+```
+<button v-on:click="add">Add</button>
+<!-- add is part of methods -->
+```
---
+++ vue/simple-single-page-event/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            score: 0,
+            name: "Hi Ben"
+        };
+    },
+    methods: {
+        increase(delta) {
+            this.score = this.score + delta;
+        },
+        decrease(delta) {
+            this.score = this.score - delta;
+        },
+        setName(event, greeting) {
+            this.name = greeting + ' ' + event.target.value;
+        }
+    }
+});
+
+app.mount('#events');
